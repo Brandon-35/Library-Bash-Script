037123e35ca2db7086559df79e2a825a8e6b93f8
BrandonTa <brandonta1035@gmail.com>
2025-02-19 01:54:41 +0700

    refactor(echo_color): update echo make color function stop ssh tunnels

--- ssh/stop_ssh_tunnels.sh
+++ ssh/stop_ssh_tunnels.sh
@@ -4,34 +4,34 @@
 tunnels=$(ps aux | grep "[s]sh -L" | awk '{print $2, $0}')
 
 if [ -z "$tunnels" ]; then
-    echo "No SSH tunnels are currently running."
+    __color yellow "No SSH tunnels are currently running."
     exit 0
 fi
 
 # Display the running SSH tunnels
-echo "The following SSH tunnels are running:"
-echo "-----------------------------------------"
+__color green "The following SSH tunnels are running:"
+__color green "-----------------------------------------"
 ps aux | grep "[s]sh -L" | awk '{print $2, $0}'
-echo "-----------------------------------------"
+__color green "-----------------------------------------"
 
 # Ask user if they want to stop all or select specific ones
 read -p "Do you want to stop all SSH tunnels? (y/n): " stop_all
 
 if [[ "$stop_all" == "y" || "$stop_all" == "Y" ]]; then
     # Kill all running SSH tunnels
-    echo "Stopping all SSH tunnels..."
+    __color yellow "Stopping all SSH tunnels..."
     ps aux | grep "[s]sh -L" | awk '{print $2}' | xargs kill
-    echo "All SSH tunnels have been stopped."
+    __color green "All SSH tunnels have been stopped."
 else
     # Allow the user to select which tunnels to stop
-    echo "Select the PID(s) of the SSH tunnels to stop (separate with space):"
+    __color green "Select the PID(s) of the SSH tunnels to stop (separate with space):"
     read -a pids_to_kill
 
     for pid in "${pids_to_kill[@]}"; do
         if kill "$pid" >/dev/null 2>&1; then
-            echo "SSH tunnel with PID $pid stopped."
+            __color green "SSH tunnel with PID $pid stopped."
         else
-            echo "Failed to stop SSH tunnel with PID $pid."
+            __color red "Failed to stop SSH tunnel with PID $pid."
         fi
     done
 fi
